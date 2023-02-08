a19fd5c9bee4db799b8804f3176dfe09e77a920f
Davidson Francis <davidsondfgl@gmail.com>
2023-02-08 13:24:43 -0300

    Adding license

--- net.h
+++ net.h
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Davidson Francis <davidsondfgl@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef NET_H
 #define NET_H
 
@@ -11,7 +35,6 @@
 		int conn, const void *buf, size_t len);
 	extern void setup_server(int *srv_fd, uint16_t port);
 	extern void setup_serial(int *sfd, const char *sdev);
-	extern void close_handled_fd(int fd);
 	extern void change_handled_fd(int fd_old,
 		struct handler_fd *new_hfd);
 	extern void handle_fds(int nfds, struct handler_fd *hfds);
